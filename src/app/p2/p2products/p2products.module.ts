d1c5d4731105eb7a0c68344ac01f077a123079f4
kvimalkr55 <kvimalkr55@gmail.com>
2020-02-07 22:08:11 +0530

    all

---
+++ src/app/p2/p2products/p2products.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { P2productsRoutingModule } from './p2products-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    P2productsRoutingModule
+  ]
+})
+export class P2productsModule { }
---
+++ src/app/p3/p3.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { P3RoutingModule } from './p3-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    P3RoutingModule
+  ]
+})
+export class P3Module { }
